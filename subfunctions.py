8bd4bb8bd9859a2501f5828645a11fbdf22c1be1
austingaskamp <austingaskamp@tamu.edu>
2021-09-19 17:29:11 -0500

    Initial function definitions

---
+++ subfunctions.py
@@ -0,0 +1,23 @@
+def get_mass(rover):
+    #Rover dictionary data structure containing rover paramters
+    """Computes the total mass of the rover. Uses information in the rover dict"""
+
+def get_gear_ratio(speed_reducer):
+    #speed_reducer must be a dictionary specifying speed reducer paramters
+
+    """Returns the speed reduction ratio for the speed reducer based on speed_reducer dict."""
+
+def tau_dcmotor(omega, motor):
+    #Omega must be numpy array
+    #Motoro must be a dictionary specifying motor parameter
+    #Must return a numpy array
+    """Returns the motor shaft torque when given motor shaft speed and a dictionarycontaining important specifications for the motor."""
+
+def F_drive():
+    """Returns the force applied to the rover by the drive systemgiven information about the drive system (wheel_assembly) and the motor shaft speed. """
+
+def F_rolling():
+    """Returns the magnitude of the force component acting on the rover in the direction of its translational motiondue  to  gravity  as  a  function  of  terrain  inclination  angle  and  rover properties"""
+
+def F_net():
+    """Returns the magnitude of net force acting on the rover in the direction of its translational motion."""
